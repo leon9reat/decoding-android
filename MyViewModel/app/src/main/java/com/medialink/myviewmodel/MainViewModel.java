40342aedbc75d79dc9c38a272596cb9f7818e376
Bambang <mail.buat.forum@gmail.com>
2019-09-30 14:13:41 +0700

    MyViewModel selesai

---
+++ MyViewModel/app/src/main/java/com/medialink/myviewmodel/MainViewModel.java
@@ -0,0 +1,63 @@
+package com.medialink.myviewmodel;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainViewModel extends ViewModel {
+    private static final String API_KEY = "f534a61d3db189a098634a352b800871";
+    private MutableLiveData<ArrayList<WeatherItems>> listWeather = new MutableLiveData<>();
+
+    public LiveData<ArrayList<WeatherItems>> getWeathers() {
+        return listWeather;
+    }
+
+    public void setWeather(final String cities) {
+        // TODO: request API
+        AsyncHttpClient client = new AsyncHttpClient();
+        final ArrayList<WeatherItems> listItems = new ArrayList<>();
+        String url = "https://api.openweathermap.org/data/2.5/group?id=" + cities
+                + "&units=metric&appid=" + API_KEY;
+
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject resObj = new JSONObject(result);
+                    JSONArray list = resObj.getJSONArray("list");
+
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject weather = list.getJSONObject(i);
+                        WeatherItems weatherItems = new WeatherItems(weather);
+                        listItems.add(weatherItems);
+                    }
+
+                    // setValue() method must be called from the main thread.
+                    // But if you need set a value from a background thread, postValue() should be used.
+                    listWeather.postValue(listItems);
+                } catch (JSONException e) {
+                    Log.d("Exception", e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("onFailure", error.getMessage());
+            }
+        });
+    }
+}
