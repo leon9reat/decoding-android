f468f5c6e3741d0d97d80401cf16df58c99dc687
leon9reat <mail.buat.forum@gmail.com>
2019-10-08 22:23:26 +0700

    init latihan menggunakan aksi dari button

---
+++ MyWidgets/app/src/main/java/com/medialink/mywidgets/NumberGenerator.java
@@ -0,0 +1,11 @@
+package com.medialink.mywidgets;
+
+import java.util.Random;
+
+public class NumberGenerator {
+
+    public static int Generate(int max) {
+        Random random = new Random();
+        return random.nextInt(max);
+    }
+}
---
+++ MyWidgets/app/src/main/java/com/medialink/mywidgets/RandomNumbersWidget.java
@@ -0,0 +1,44 @@
+package com.medialink.mywidgets;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.text.TextUtils;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class RandomNumbersWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        String lastUpdate = String.format("Random: %d", NumberGenerator.Generate(100));
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.random_numbers_widget);
+        views.setTextViewText(R.id.appwidget_text, lastUpdate);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
