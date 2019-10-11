833b38a493035910111b59b817a248f467ee8298
Bambang <mail.buat.forum@gmail.com>
2019-10-11 11:28:30 +0700

    stack notif selesai

--- StackNotif/app/src/main/java/com/medialink/stacknotif/MainActivity.java
+++ StackNotif/app/src/main/java/com/medialink/stacknotif/MainActivity.java
@@ -3,12 +3,15 @@ package com.medialink.stacknotif;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -71,7 +74,59 @@ public class MainActivity extends AppCompatActivity {
 
         NotificationCompat.Builder mBuilder;
 
-        // TODO: lanjutkan lagi besok
+        // Melakukan pengecekan jika idNotification lebih kecil dari Max Notif
+        String CHANNEL_ID = "channel_01";
+        if (idNotification < MAX_NOTIFICATION) {
+            mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("New Email from " + stackNotif.get(idNotification).getSender())
+                    .setContentText(stackNotif.get(idNotification).getMessage())
+                    .setSmallIcon(R.drawable.ic_email_white_24dp)
+                    .setLargeIcon(largeIcon)
+                    .setGroup(GROUP_KEY_EMAILS)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+        } else {
+            NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle()
+                    .addLine("New Email from " + stackNotif.get(idNotification).getSender())
+                    .addLine("New Email from " + stackNotif.get(idNotification - 1).getSender())
+                    .setBigContentTitle(idNotification + " new emails")
+                    .setSummaryText("mail@dicoding");
+            mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle(idNotification + " new emails")
+                    .setContentText("mail@dicoding.com")
+                    .setSmallIcon(R.drawable.ic_email_white_24dp)
+                    .setGroup(GROUP_KEY_EMAILS)
+                    .setGroupSummary(true)
+                    .setContentIntent(pendingIntent)
+                    .setStyle(inboxStyle)
+                    .setAutoCancel(true);
+        }
 
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel
+        Materi ini akan dibahas lebih lanjut di modul extended
+        */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            mBuilder.setChannelId(CHANNEL_ID);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        Notification notification = mBuilder.build();
+        if (notificationManager != null) {
+            notificationManager.notify(idNotification, notification);
+        }
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        stackNotif.clear();
+        idNotification = 0;
     }
 }
