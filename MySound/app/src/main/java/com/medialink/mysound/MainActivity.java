89564813b9414e0a0c9cb3a977333400b73bceb0
Bambang <mail.buat.forum@gmail.com>
2019-10-12 12:02:37 +0700

    init soundpool

---
+++ MySound/app/src/main/java/com/medialink/mysound/MainActivity.java
@@ -0,0 +1,50 @@
+package com.medialink.mysound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSound;
+    SoundPool sp;
+    int soundId;
+    boolean spLoaded = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSound = findViewById(R.id.btn_soundpool);
+        sp = new SoundPool.Builder()
+                .setMaxStreams(10)
+                .build();
+        sp.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                if (status == 0) {
+                    spLoaded = true;
+                } else {
+                    Toast.makeText(MainActivity.this, "Gagal load", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        soundId = sp.load(this, R.raw.nexttime, 1);
+
+        btnSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (spLoaded) {
+                    sp.play(soundId, 1, 1, 0, 0, 1);
+                }
+            }
+        });
+    }
+}
