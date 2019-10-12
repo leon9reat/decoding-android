71032212a35c3465172639694940065ca23c3525
Bambang <mail.buat.forum@gmail.com>
2019-10-12 11:16:49 +0700

    init and end web view

---
+++ MyWebView/app/src/main/java/com/medialink/mywebview/MainActivity.java
@@ -0,0 +1,47 @@
+package com.medialink.mywebview;
+
+import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.webView)
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')");
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                result.confirm();
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
+
+        webView.loadUrl("https://www.dicoding.com");
+    }
+}
